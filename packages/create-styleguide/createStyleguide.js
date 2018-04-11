41caddd3e4f8546b8eee2a7ec3b51007f2f36bd1
AngryNui <Nui.liehr@gmail.com>
2018-04-11 13:43:14 +0200

    add argument styleguideName and creating folders

--- packages/create-styleguide/createStyleguide.js
+++ packages/create-styleguide/createStyleguide.js
@@ -1,7 +1,34 @@
-
-
 'use strict';
 
+const fs = require('fs')
+const path = require('path')
 const chalk = require('chalk');
 
+const mkdirSync = function (dirPath) {
+    try {
+        fs.mkdirSync(dirPath)
+    } catch (err) {
+        console.log(chalk.red(dirPath +'already excists'));
+        if (err.code !== 'EEXIST') throw err
+    }
+}
+
+let styleguideName = process.argv[2];
+let appPath = "./"+styleguideName;
+
+if (!styleguideName){
+    console.log(chalk.red('projectname is missing :('));
+    console.log(chalk.green(''));
+    console.log(chalk.green('Try:'));
+    console.log(chalk.green('create-styleguide my-awesome-styleguide'));
+    process.exit(1);
+}
+
 console.log(chalk.green('// ToDo: create styleguide :D'));
+console.log(chalk.green('create ./' + styleguideName));
+
+// creating folders
+mkdirSync(path.resolve('./'+styleguideName));
+mkdirSync(path.resolve(appPath+"/app"));
+mkdirSync(path.resolve(appPath+"/src"));
+mkdirSync(path.resolve(appPath+"/node_modules"));
